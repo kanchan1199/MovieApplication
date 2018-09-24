566868e626e04593737d2bd99a0526e8d8b438f1
anurag <anuragkanchan.viet@gmail.com>
2018-09-24 18:39:07 +0530

    secnd commit

--- src/com/movie/Main.java
+++ src/com/movie/Main.java
@@ -3,7 +3,7 @@ package com.movie;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Movie Application");
+		System.out.println("Movie Application GIT");
 
 	}
 
